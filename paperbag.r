7e9acbdb340b62a19389c9121fa4cc4b455ad49a
abilouhill <abilouhill@gmail.com>
2015-08-07 11:33:21 +1000

    test

---
+++ paperbag.r
@@ -0,0 +1,18 @@
+#' Coding is like being blindfolded in a paperbag
+#' 
+#' This is practice for writing packages. This would be where the description going
+#' @name Paperbag
+#' @author A. Proctor
+#' @docType package
+#' 
+#' @param x A parameter for a function
+#' @param y A paramter
+#' 
+#' @return The output will be a dingle integer
+#' @export adding
+#' @example adding(1, 1)
+adding <- function(x,y){
+  x +y
+  
+}
+
